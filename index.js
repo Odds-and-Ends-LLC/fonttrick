c6598beda1e2a66e8f4fbbd8ef3f8a91d8f433d3
codeninjadevt <dryl.dionisio@gmail.com>
2024-08-02 19:53:43 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+module.exports = function fonttrick() {
+  const fs = require('fs')
+  const path = require("path");
+
+  const { COPYFILE_EXCL } = fs.constants;
+  const { COPYFILE_FICLONE } = fs.constants;
+
+  const fonts = [
+    { fontFamily: "Chelsea Market", path: "ChelseaMarket-Regular.ttf" },
+    { fontFamily: "Pacifico", path: "Pacifico-Regular.ttf" },
+    { fontFamily: "Coconut House", path: "Coconut House.ttf" },
+    { fontFamily: "Aloha", path: "Aloha.ttf" },
+    { fontFamily: "Wakiki", path: "Wakiki.ttf" },
+    { fontFamily: "Futura Condensed", path: "Futura Condensed Extra Bold.otf" },
+    { fontFamily: "Surfing Capital", path: "Surfing Capital.ttf" },
+  ];
+  const fontList = {};
+
+  fonts.forEach(font => {
+    if (fs.existsSync(`/tmp/ChelseaMarket-Regular.ttf`)) {
+      console.log(`ChelseaMarket-Regular.ttf lives in tmp!!!!`)
+    } else {
+      const FontFile = require.resolve(`./fonts/${font}`)
+
+      fs.copyFileSync(FontFile, `/tmp/${font}`, COPYFILE_FICLONE | COPYFILE_EXCL);
+
+      fontList[font.fontFamily] = `/tmp/${font}`;
+    }
+  })
+
+  return fontList;
+};
